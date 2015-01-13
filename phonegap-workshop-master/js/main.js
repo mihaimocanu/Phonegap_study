7726ff7d124bc6f6c32c9897c553239ce4265716
Mihai Mocanu <v-mmo@futurecom.local>
2015-01-13 16:40:59 +0200

    Change the logic that the layout is created

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -1,15 +1,9 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+        var self = this;
+        this.store.findByName($('.search-key').val(), function (employees) {
+            $('.employee-list').html(self.employeeLiTpl(employees));
         });
     },
 
@@ -23,12 +17,19 @@ var app = {
         }
     },
 
+    renderHomeView: function(){
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
     initialize: function () {
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         var self = this;
         this.store = new MemoryStore(function () {
-            self.showAlert('Store Initialized', 'Info');
+            self.renderHomeView();
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        
     }
 
 };
