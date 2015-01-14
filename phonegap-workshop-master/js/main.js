6d487f7248d967c530d5c9b0af842fc83fece5ab
Mihai Mocanu <v-mmo@futurecom.local>
2015-01-14 11:12:08 +0200

    Added alerts for debugging

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -17,19 +17,27 @@ var app = {
         }
     },
 
-    renderHomeView: function(){
+    renderHomeView: function () {
+        
         $('body').html(this.homeTpl());
+        this.showAlert('RenderHomeView-Body', 'Info');
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        this.showAlert('RenderHomeView-Search', 'Info');
     },
 
     initialize: function () {
+        this.showAlert('Initializare-Start', 'Info');
         this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.showAlert('Initializare-Added home Template', 'Info');
         this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        this.showAlert('Initializare-Added Employee Template', 'Info');
         var self = this;
         this.store = new MemoryStore(function () {
+            self.showAlert('RenderHomeView-Start', 'Info');
             self.renderHomeView();
+            self.showAlert('RenderHomeView-Finish', 'Info');
         });
-        
+        this.showAlert('Initializare-Finish', 'Info');
     }
 
 };
